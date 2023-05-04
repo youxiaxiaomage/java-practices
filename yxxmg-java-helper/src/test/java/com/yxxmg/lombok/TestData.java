4c0ad996cced73f46fd1364dbdffe615230472c4
youxiaxiaomage <yanzhao_jn@163.com>
2023-05-04 09:43:05 +0800

    :white_check_mark: lombok staticName function

---
+++ yxxmg-java-helper/src/test/java/com/yxxmg/lombok/TestData.java
@@ -0,0 +1,33 @@
+package com.yxxmg.lombok;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.Serializable;
+
+/**
+ * @author : yxxmg
+ * @version : 1.0
+ * @description :
+ * @since : 2023/5/4
+ */
+@RunWith(JUnit4.class)
+public class TestData {
+    @Test
+    public void test() {
+        Data of = Data.of("1", "张三");
+        Assert.assertEquals("1", of.getId());
+    }
+
+    @lombok.Data
+    @AllArgsConstructor(staticName = "of", access = AccessLevel.PRIVATE)
+    static class Data implements Serializable {
+        private static final long serialVersionUID = 699687860242579358L;
+        private String id;
+        private String name;
+    }
+}
