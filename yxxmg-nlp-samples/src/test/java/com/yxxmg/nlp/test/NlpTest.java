040ef2ec47fd622cdf50a142444d9c05a1e8366a
youxiaxiaomage <yanzhao_jn@163.com>
2023-05-25 14:34:58 +0800

    :sparkles: add nlp, GC overhead limit exceeded

---
+++ yxxmg-nlp-samples/src/test/java/com/yxxmg/nlp/test/NlpTest.java
@@ -0,0 +1,105 @@
+package com.yxxmg.nlp.test;
+
+import com.google.common.io.Files;
+import edu.stanford.nlp.coref.CorefCoreAnnotations;
+import edu.stanford.nlp.coref.data.CorefChain;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeCoreAnnotations;
+import edu.stanford.nlp.util.CoreMap;
+import edu.stanford.nlp.util.PropertiesUtils;
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * @author : yxxmg
+ * @version : 1.0
+ * @description :
+ * @since : 2023/5/25
+ */
+public class NlpTest extends TestCase {
+
+    public void test1() throws IOException {
+        // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution
+        Properties props = new Properties();
+        props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        // read some text from the file..
+        File inputFile = new File("src/test/resources/sample-content.txt");
+        String text = Files.asCharSource(inputFile, StandardCharsets.UTF_8).read();
+
+        // create an empty Annotation just with the given text
+        Annotation document = new Annotation(text);
+
+        // run all Annotators on this text
+        pipeline.annotate(document);
+
+        // these are all the sentences in this document
+        // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
+        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+
+        for (CoreMap sentence : sentences) {
+            // traversing the words in the current sentence
+            // a CoreLabel is a CoreMap with additional token-specific methods
+            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+                // this is the text of the token
+                String word = token.get(CoreAnnotations.TextAnnotation.class);
+                // this is the POS tag of the token
+                String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+                // this is the NER label of the token
+                String ne = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
+
+                System.out.println("word: " + word + " pos: " + pos + " ne:" + ne);
+            }
+
+            // this is the parse tree of the current sentence
+            Tree tree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
+            System.out.println("parse tree:\n" + tree);
+
+            // this is the Stanford dependency graph of the current sentence
+            SemanticGraph dependencies =
+                sentence.get(SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation.class);
+            System.out.println("dependency graph:\n" + dependencies);
+        }
+
+        // This is the coreference link graph
+        // Each chain stores a set of mentions that link to each other,
+        // along with a method for getting the most representative mention
+        // Both sentence and token offsets start at 1!
+        Map<Integer, CorefChain> graph = document.get(CorefCoreAnnotations.CorefChainAnnotation.class);
+
+    }
+
+    public void test2() {
+        String props = "CoreNLP-chinese.properties"; // 第三步骤的配置文件，放在main/resources目录下
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        Annotation annotation = new Annotation("这家酒店很好，我很喜欢。");
+        pipeline.annotate(annotation);
+        List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+        // ArrayList<String> array = new ArrayList<String>();
+        StringBuffer sb = new StringBuffer();
+        for (CoreMap sentence : sentences) {
+            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+                String word = token.get(CoreAnnotations.TextAnnotation.class);
+                sb.append(word);
+                sb.append(" ");
+            }
+        }
+        System.out.println(sb.toString().trim());
+
+    }
+}
