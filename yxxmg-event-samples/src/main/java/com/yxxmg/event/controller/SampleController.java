7fd16d9203c70707b86d7a784602057d8c30cf8f
youxiaxiaomage <yanzhao_jn@163.com>
2023-06-06 10:36:37 +0800

    fix: 代码错误

--- yxxmg-event-samples/src/main/java/com/yxxmg/event/controller/SampleController.java
+++ yxxmg-event-samples/src/main/java/com/yxxmg/event/controller/SampleController.java
@@ -1,16 +1,21 @@
 package com.yxxmg.event.controller;
 
+import com.yxxmg.event.service.NotifyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author : yxxmg
  * @version : 1.0
- * @description : TODO
+ * @description :
  * @since : 2023/6/6
  */
 @RestController
+@RequiredArgsConstructor
 public class SampleController {
-    public String notify(String msg) {
+    private final NotifyService notifyService;
 
+    public String notify(String msg) {
+        return this.notifyService.notify(msg);
     }
 }
