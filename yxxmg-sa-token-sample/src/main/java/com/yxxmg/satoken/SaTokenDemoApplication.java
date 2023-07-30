c73f7ca3c9caa37573cac753cc04c516a6167338
youxiaxiaomage <yanzhao_jn@163.com>
2023-07-30 22:06:31 +0800

    feat: add sa-token sample

---
+++ yxxmg-sa-token-sample/src/main/java/com/yxxmg/satoken/SaTokenDemoApplication.java
@@ -0,0 +1,17 @@
+package com.yxxmg.satoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : yxxmg
+ * @version : 1.0
+ * @description :
+ * @since : 2023/7/30
+ */
+@SpringBootApplication
+public class SaTokenDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SaTokenDemoApplication.class, args);
+    }
+}
