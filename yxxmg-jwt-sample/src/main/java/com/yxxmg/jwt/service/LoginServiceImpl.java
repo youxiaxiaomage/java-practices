c867fef484da4f5e53089e398ca4166af3a04a4f
youxiaxiaomage <yanzhao_jn@163.com>
2023-10-18 09:57:56 +0800

    :sparkles: 优化配置

--- yxxmg-jwt-sample/src/main/java/com/yxxmg/jwt/service/LoginServiceImpl.java
+++ yxxmg-jwt-sample/src/main/java/com/yxxmg/jwt/service/LoginServiceImpl.java
@@ -1,16 +1,16 @@
 package com.yxxmg.jwt.service;
 
-import com.yxxmg.jwt.api.LoginService;
-import com.yxxmg.jwt.dto.LoginDTO;
-import com.yxxmg.jwt.entity.User;
-import com.yxxmg.jwt.utils.JwtUtils;
-import com.yxxmg.jwt.vo.TokenVO;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import com.yxxmg.jwt.api.LoginService;
+import com.yxxmg.jwt.dto.LoginDTO;
+import com.yxxmg.jwt.entity.User;
+import com.yxxmg.jwt.vo.TokenVO;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * @author : yxxmg
@@ -22,15 +22,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LoginServiceImpl implements LoginService {
     private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+
     @Override
     public TokenVO login(LoginDTO loginDTO) {
         Authentication authentication = this.authenticationManager
-                .authenticate(new UsernamePasswordAuthenticationToken(loginDTO.getUserName(), loginDTO.getPassword()));
-
-
-        return TokenVO
-                .builder()
-                .token(JwtUtils.createAccessToken((User) authentication.getPrincipal()))
-                .build();
+            .authenticate(new UsernamePasswordAuthenticationToken(loginDTO.getUserName(), loginDTO.getPassword()));
+        return TokenVO.builder().token(this.tokenService.generateToken((User)authentication.getPrincipal())).build();
     }
 }
