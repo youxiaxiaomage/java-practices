1a74a52512561364957e2227b65134a9a3552b7c
zhaoyan <zhaoyan@jsleasing.cn>
2023-12-18 17:17:36 +0800

    :white_check_mark: Arrays.asList()验证

---
+++ yxxmg-java-helper/src/test/java/com/yxxmg/collection/CollectionTest.java
@@ -0,0 +1,38 @@
+package com.yxxmg.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * @author : yxxmg
+ * @version : 1.0
+ * @description :
+ * @since : 2023/12/18
+ */
+@RunWith(JUnit4.class)
+public class CollectionTest {
+    @Test
+    public void test() {
+        // List<String> l1 = Arrays.asList("1", "2");
+        // l1.add("2");
+        // System.out.println(l1);
+        // List<String> l2 = Lists.newArrayList("1", "2");
+        // l2.add("3");
+        // System.out.println(l2);
+
+        // List<String> l3 = Stream.of("1", "2").collect(Collectors.toList());
+        // l3.add("3");
+        // System.out.println(l3);
+        // List<String> l4 = Arrays.stream(new String[] {"1", "2"}).collect(Collectors.toList());
+        // l4.add("3");
+        // System.out.println(l4);
+        List<String> l5 = new ArrayList<>(Arrays.asList("1", "2"));
+        l5.add("3");
+        System.out.println(l5);
+    }
+}
