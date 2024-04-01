f28d601392f0f0633409e9b23475ee1051c6ac8a
youxiaxiaomage <yanzhao_jn@163.com>
2024-04-01 20:55:22 +0800

    :zap: 脚手架优化

--- yxxmg-archetype-sample/src/main/resources/archetype-resources/__rootArtifactId__-boot/src.main/java/YxxmgApplication.java
+++
@@ -1,7 +0,0 @@
-package ${package};
-
-public class YxxmgApplication {
-    public static void main(String[] args) {
-        System.out.println("YxxmgApplication starting");
-    }
-}
---
+++ yxxmg-archetype-sample/src/main/resources/archetype-resources/__rootArtifactId__-infrastructure/__rootArtificatId-mybatis/com.yxxmg.mybatis/CustomMybatisConfiguration.java
@@ -0,0 +1,6 @@
+package com.yxxmg.mybatis;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomMybatisConfiguration {}
