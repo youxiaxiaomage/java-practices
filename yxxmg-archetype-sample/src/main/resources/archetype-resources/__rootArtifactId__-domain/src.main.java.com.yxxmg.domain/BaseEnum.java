fca4932395d8b91b355c7b97e0f4f63407f119c8
youxiaxiaomage <yanzhao_jn@163.com>
2024-04-03 10:02:15 +0800

    :zap: fix bugs

--- yxxmg-archetype-sample/src/main/resources/archetype-resources/__rootArtifactId__-domain/src.main.java.com.yxxmg.domain/BaseEnum.java
+++
@@ -1,5 +0,0 @@
-package com.yxxmg.domain;
-
-public enum BaseEnum {
-    ENABLE, DISABLE;
-}
---
+++ yxxmg-archetype-sample/src/main/resources/archetype-resources/__rootArtifactId__-infrastructure/__rootArtificatId__-mybatis/src/main/java/com/yxxmg/mybatis/entity/BaseEntity.java
@@ -0,0 +1,8 @@
+package com.yxxmg.mybatis.entity;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = 2105685714824875990L;
+}
