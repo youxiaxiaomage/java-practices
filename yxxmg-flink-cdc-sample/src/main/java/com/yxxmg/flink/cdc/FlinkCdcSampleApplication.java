158b40ded30debd9dfd316196055a2881ff6c69c
youxiaxiaomage <yanzhao_jn@163.com>
2024-10-08 11:23:34 +0800

    docs: 添加注释

--- yxxmg-flink-cdc-sample/src/main/java/com/yxxmg/flink/cdc/FlinkCdcSampleApplication.java
+++ yxxmg-flink-cdc-sample/src/main/java/com/yxxmg/flink/cdc/FlinkCdcSampleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author : yxxmg
  * @version : 1.0
- * @description :启动类
+ * @description :启动
  * @since : 2024/8/13
  */
 @SpringBootApplication
